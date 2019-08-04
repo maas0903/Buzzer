d3ef81726d4c9ca7416cabe1b75a58bf3acfea9b
maas0903 <marius.tennapel@hotmail.com>
2019-08-04 14:13:20 +0200

    added passive, using SoftTone from wiringPi

--- src/buzzer/Buzzer.java
+++ src/buzzer/Buzzer.java
@@ -14,11 +14,17 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.wiringpi.Gpio;
+import com.pi4j.wiringpi.SoftTone;
 import java.time.LocalTime;
 
 public class Buzzer
 {
-    public static void ActiveBuzzer(GpioPinDigitalOutput buzzerPin) throws InterruptedException{
+
+    public static void ActiveBuzzer(GpioController gpio) throws InterruptedException
+    {
+        final GpioPinDigitalOutput buzzerPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "GPIO_00_On_GPIO17", PinState.LOW);
+        buzzerPin.setShutdownOptions(true, PinState.LOW);
         LocalTime time = LocalTime.now();
         LocalTime endTime = time.plusSeconds(2);
         boolean keepRunning = true;
@@ -36,9 +42,19 @@ public class Buzzer
             }
         }
     }
-    
-    public static void PassiveBuzzer(GpioPinDigitalOutput buzzerPin) throws InterruptedException{
-        
+
+    public static void PassiveBuzzer() throws InterruptedException
+    {
+        Gpio.wiringPiSetup();
+        SoftTone.softToneCreate(0);
+        //uses the WiringPi pin (http://wiringpi.com/pins/)
+        //0 == RaspiPin.GPIO_00 == BCM GPIO17
+        int pin = 0;
+        //frequency between 0 (off) and 5000Hz
+        int frequency = 330;
+        SoftTone.softToneWrite(pin, frequency);
+        Thread.sleep(1000);
+        SoftTone.softToneWrite(pin, 0);
     }
 
     /**
@@ -46,9 +62,7 @@ public class Buzzer
      */
     public static void main(String[] args) throws InterruptedException
     {
-        final GpioController gpio = GpioFactory.getInstance();
-        final GpioPinDigitalOutput buzzerPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "GPIO_00_On_GPIO17", PinState.LOW);
-        buzzerPin.setShutdownOptions(true, PinState.LOW);
-        ActiveBuzzer(buzzerPin);
+        //ActiveBuzzer(GpioFactory.getInstance());
+        PassiveBuzzer();
     }
 }
